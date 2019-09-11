799c2bfc7ed605a3ad6e694f815edffcc0b307f1
Rasmus Eneman <rasmus@eneman.eu>
2019-09-11 22:42:41 +0200

    Add basic calendar view

--- src/clock.rs
+++ src/clock.rs
@@ -1,13 +1,25 @@
 use crate::clone;
+use crate::popup::create_popup;
 use crate::utils::format_panel_text;
 use chrono::Local;
 use gtk::prelude::*;
 
 fn current_time() -> String {
-  format_panel_text(Local::now().format("%h %d %H:%M:%S"))
+  format_panel_text(Local::now().format("%h %d %H:%M"))
 }
 
-pub fn create_clock() -> gtk::Label {
+fn create_time_menu() -> impl gtk::IsA<gtk::Widget> {
+  let calendar = gtk::CalendarBuilder::new()
+    .margin(16)
+    .expand(true)
+    .show_heading(true)
+    .show_week_numbers(true)
+    .build();
+
+  calendar
+}
+
+pub fn create_clock() -> impl gtk::IsA<gtk::Widget> {
   let label = gtk::Label::new(None);
   label.set_margin_top(6);
   label.set_margin_bottom(6);
@@ -20,5 +32,16 @@ pub fn create_clock() -> gtk::Label {
 
   gtk::timeout_add_seconds(1, tick);
 
-  label
+  let time_button = gtk::EventBox::new();
+  time_button.add(&label);
+
+  time_button.connect_button_press_event(move |time_button, _| {
+    let time_menu = create_time_menu();
+    let show_popup = create_popup(time_button, &time_menu);
+
+    show_popup();
+    Inhibit(false)
+  });
+
+  time_button
 }
